85c4ec299c006709621d49ea71aa58f1ba229c38
Rômulo Eduardo Garcia Moraes <romulo.moraes@stone.com.br>
2019-11-22 15:43:23 -0300

    Modifica super classe de testes de cartão para AutoLoadTableTest
    A modificação foi feita para garantir que os testes de cartão sempre
    rodem com as tabelas atualizadas.
    
    Signed-off-by: Rômulo Eduardo Garcia Moraes <romulo.moraes@stone.com.br>

--- app/src/androidTest/java/br/com/stone/posandroid/hal/demo/bc/CardCommandsTest.kt
+++ app/src/androidTest/java/br/com/stone/posandroid/hal/demo/bc/CardCommandsTest.kt
@@ -6,8 +6,7 @@ import br.com.stone.posandroid.hal.api.bc.PinpadCallbacks.Companion.PROCESSING
 import br.com.stone.posandroid.hal.api.bc.PinpadResult
 import br.com.stone.posandroid.hal.api.bc.PinpadResultCallback
 import br.com.stone.posandroid.hal.api.bc.constants.ResultCode.Companion.PP_OK
-import br.com.stone.posandroid.hal.demo.bc.base.AutoLoadTableTest.Companion.TABLE_STUB_TIMESTAMP
-import br.com.stone.posandroid.hal.demo.bc.base.AutoOpenCloseTest
+import br.com.stone.posandroid.hal.demo.bc.base.AutoLoadTableTest
 import br.com.stone.posandroid.hal.demo.util.blockingAssertions
 import io.mockk.verifySequence
 import org.junit.Assert.assertEquals
@@ -15,7 +14,7 @@ import org.junit.Assert.assertTrue
 import org.junit.Test
 
 
-class CardCommandsTest : AutoOpenCloseTest() {
+class CardCommandsTest : AutoLoadTableTest() {
 
     private val stubResultsFolder = "card-commands-test"
 
