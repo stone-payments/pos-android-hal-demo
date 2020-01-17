e232f66ea7a37ccf2928cfe525a9b06850a3580c
TiagoCabral <tiago.gnr1985@gmail.com>
2020-01-17 19:15:02 -0300

    Adds Gertec DeviceProvider as comment.

--- app/src/androidTest/java/br/com/stone/posandroid/hal/demo/HALConfig.kt
+++ app/src/androidTest/java/br/com/stone/posandroid/hal/demo/HALConfig.kt
@@ -1,11 +1,13 @@
 package br.com.stone.posandroid.hal.demo
 
 import br.com.stone.posandroid.hal.api.provider.DeviceProvider
+//import br.com.stone.posandroid.hal.gertec.provider.GertecDeviceProvider
 import br.com.stone.posandroid.hal.mock.provider.MockDependencyProvider
 
 object HALConfig {
     // TODO change to your Provider
     val deviceProvider: DeviceProvider = MockDependencyProvider()
+//    val deviceProvider: DeviceProvider = GertecDeviceProvider
 
     val runningOnEmulator = deviceProvider is MockDependencyProvider
 }
