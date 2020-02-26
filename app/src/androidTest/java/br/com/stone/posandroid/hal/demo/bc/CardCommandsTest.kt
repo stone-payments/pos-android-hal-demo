d3010107133a393536e0107fcfeb57e615b33505
Rafael Itajubá <rafael.rabelo.it@gmail.com>
2020-02-26 15:24:08 -0300

    getCard and getPIN tests

--- app/src/androidTest/java/br/com/stone/posandroid/hal/demo/bc/CardCommandsTest.kt
+++ app/src/androidTest/java/br/com/stone/posandroid/hal/demo/bc/CardCommandsTest.kt
@@ -3,14 +3,11 @@ package br.com.stone.posandroid.hal.demo.bc
 import br.com.stone.posandroid.hal.api.Properties.RESULTS_FILE_KEY
 import br.com.stone.posandroid.hal.api.bc.PinpadCallbacks.Companion.INSERT_SWIPE_CARD
 import br.com.stone.posandroid.hal.api.bc.PinpadCallbacks.Companion.PROCESSING
-import br.com.stone.posandroid.hal.api.bc.PinpadResult
-import br.com.stone.posandroid.hal.api.bc.PinpadResultCallback
-import br.com.stone.posandroid.hal.api.bc.constants.ResultCode.Companion.PP_OK
+import br.com.stone.posandroid.hal.api.bc.ext.getCardOrThrows
 import br.com.stone.posandroid.hal.api.bc.ext.removeCardOrThrows
 import br.com.stone.posandroid.hal.demo.bc.base.AutoLoadTableTest
 import br.com.stone.posandroid.hal.demo.util.DEFAULT_GCR_INPUT
 import br.com.stone.posandroid.hal.demo.util.VISA_TESTCARD01_OUTPUT
-import br.com.stone.posandroid.hal.demo.util.blockingAssertions
 import io.mockk.verifySequence
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
@@ -23,37 +20,17 @@ class CardCommandsTest : AutoLoadTableTest() {
     private val stubResultsFolder = "resources/card-commands-test"
 
     @Test
-    fun validateGetCard() {
+    fun validateGetCard() = runBlocking {
 
         pinpad.runtimeProperties[RESULTS_FILE_KEY] =
             "$stubResultsFolder/validate_get_card.json"
 
-        val pinpadResultAssertions = { pinpadResult: PinpadResult ->
+        assertEquals(VISA_TESTCARD01_OUTPUT, pinpad.getCardOrThrows(DEFAULT_GCR_INPUT))
 
-            val expectedOutput = VISA_TESTCARD01_OUTPUT
-            assertEquals(PP_OK, pinpadResult.resultCode)
-            assertEquals(expectedOutput, pinpadResult.output)
-
-            verifySequence {
-                callback.onEvent(INSERT_SWIPE_CARD, "")
-                callback.onEvent(PROCESSING, "")
-            }
+        verifySequence {
+            callback.onEvent(INSERT_SWIPE_CARD, "")
+            callback.onEvent(PROCESSING, "")
         }
-
-        val pinpadCommandsAssertions = { resultCallback: PinpadResultCallback ->
-            assertEquals(
-                PP_OK,
-                pinpad.getCard(
-                    DEFAULT_GCR_INPUT,
-                    resultCallback
-                )
-            )
-        }
-
-        blockingAssertions(
-            pinpadResultAssertions,
-            pinpadCommandsAssertions
-        )
     }
 
     @Test
