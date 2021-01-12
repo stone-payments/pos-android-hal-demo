37fb6ccb817d3fcc51a3f4f9e8b9ef1b2b835036
Rafael Itajubá <rafael.rabelo.it@gmail.com>
2021-01-12 15:26:28 -0300

    remove old util to test async

--- app/src/androidTest/java/br/com/stone/posandroid/hal/demo/util/TestUtils.kt
+++ app/src/androidTest/java/br/com/stone/posandroid/hal/demo/util/TestUtils.kt
@@ -1,39 +1,6 @@
 package br.com.stone.posandroid.hal.demo.util
 
-import br.com.stone.posandroid.hal.api.printer.PrintCallback
-import java.util.concurrent.Semaphore
-import java.util.concurrent.TimeUnit
-
-/**
- *
- */
-fun blockingPrinterAssertions(
-    successAssertions: () -> Unit,
-    errorAssertions: (Int) -> Unit,
-    functionAssertions: (PrintCallback) -> Unit
-) {
-
-    val semaphore = Semaphore(0)
-
-    val printCallback = object : PrintCallback {
-        override fun onSuccess() {
-            successAssertions()
-            semaphore.release()
-        }
-
-        override fun onError(errorCode: Int) {
-            errorAssertions(errorCode)
-            semaphore.release()
-        }
-    }
-
-    functionAssertions(printCallback)
-
-    semaphore.tryAcquire(100, TimeUnit.MILLISECONDS)
-}
-
 fun String.isValidHex(expectedSize: Int = 0): Boolean {
-
     val patternSize = if (expectedSize == 0) "+" else expectedSize.toString()
     val pattern = "[0-9A-F]{${patternSize}}"
 
