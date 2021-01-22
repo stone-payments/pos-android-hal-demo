881d23e257370b37271b8b85f4ff4cac538263ee
Gabriel Xavier <gabriel.xsilva@stone.com.br>
2021-01-22 12:25:37 -0300

    [AB#229614] - create test kernelVersion in DeviceInfoTest.kt

---
+++ app/src/androidTest/java/br/com/stone/posandroid/hal/demo/settings/DeviceInfoTest.kt
@@ -0,0 +1,36 @@
+package br.com.stone.posandroid.hal.demo.settings
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.platform.app.InstrumentationRegistry
+import br.com.stone.posandroid.hal.api.Properties
+import br.com.stone.posandroid.hal.api.settings.DeviceInfo
+import br.com.stone.posandroid.hal.demo.HALConfig
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@Suppress("TestFunctionName")
+@RunWith(AndroidJUnit4ClassRunner::class)
+class DeviceInfoTest {
+
+    private val stubResultsFolder = "resources/settings/settings-test"
+    private val context by lazy { InstrumentationRegistry.getInstrumentation().targetContext }
+
+    private val deviceInfo by lazy {
+        HALConfig.deviceProvider.getSettings(
+            mapOf(
+                Properties.RESULTS_FILE_KEY to "$stubResultsFolder/settings-deviceinfo.json",
+                Properties.KEY_CONTEXT to context
+            )
+        ).getDeviceInfo()
+    }
+
+    @Test
+    fun When_accessed_the_kernel_version_Should_return_a_valid_value() {
+        val kernelVersion = deviceInfo.kernelVersion
+        val result = kernelVersion.isEmpty().not() && kernelVersion != DeviceInfo.KERNEL_UNKNOWN
+        Assert.assertEquals(true, result)
+    }
+
+
+}
