e129a9c5099e0a72cb7f9a7530738450b6180b67
Leandro Bastos <17bastos@gmail.com>
2021-03-22 15:14:22 -0300

    :bug: Fix HALConfig to use AutoProvider

--- app/src/main/java/br/com/stone/posandroid/hal/demo/HALConfig.kt
+++ app/src/main/java/br/com/stone/posandroid/hal/demo/HALConfig.kt
@@ -3,9 +3,8 @@ package br.com.stone.posandroid.hal.demo
 import br.com.stone.posandroid.hal.api.provider.AutoProvider
 import br.com.stone.posandroid.hal.api.provider.DeviceProvider
 import br.com.stone.posandroid.hal.mock.provider.MockDeviceProvider
-import br.com.stone.posandroid.hal.sunmi.provider.StoneDeviceProvider
 
 object HALConfig {
-    val deviceProvider: DeviceProvider = StoneDeviceProvider()
+    val deviceProvider: DeviceProvider = AutoProvider.provider
     val runningOnEmulator = deviceProvider is MockDeviceProvider
 }
