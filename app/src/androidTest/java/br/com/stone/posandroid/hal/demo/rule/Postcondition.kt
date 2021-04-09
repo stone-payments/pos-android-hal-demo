567fe46b9d8384b4de49c0a6044b09c927107ba4
Gabriel Xavier <gabriel.xs.silva39@gmail.com>
2021-04-09 12:15:40 -0300

    Create anotation PostCondition

---
+++ app/src/androidTest/java/br/com/stone/posandroid/hal/demo/rule/Postcondition.kt
@@ -0,0 +1,10 @@
+package br.com.stone.posandroid.hal.demo.rule
+
+import androidx.test.rule.ActivityTestRule
+
+/**
+ * Annotation used to indicate the precondition that must be met after proceeding with test execution.
+ *
+ * This annotation is used in conjuction with [PostconditionTestRule].
+ */
+annotation class Postcondition(val text: String)
---
+++ app/src/androidTest/java/br/com/stone/posandroid/hal/demo/rule/PostconditionTestRule.kt
@@ -0,0 +1,64 @@
+package br.com.stone.posandroid.hal.demo.rule
+
+import android.content.Intent
+import android.widget.Button
+import android.widget.TextView
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import br.com.stone.posandroid.hal.demo.HALConfig
+import br.com.stone.posandroid.hal.demo.MainActivity
+import br.com.stone.posandroid.hal.demo.R
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+import java.util.concurrent.Semaphore
+
+
+/**
+ * This rule provides a way of blocking the execution of test cases by showing a screen with a
+ * [Postcondition] message and a button to proceed with the execution.
+ *
+ * The use case of this rule is when executing tests on physical devices. Since some tests
+ * depends on "hard to emulate" device states (printer without paper, low battery etc.) we need to
+ * ensure that the device meet these conditions before proceeding with test execution, preventing
+ * test failures.
+ *
+ */
+
+class PostconditionTestRule :
+    ActivityTestRule<MainActivity>(MainActivity::class.java, false, false) {
+
+    /**
+     * Block the execution of the test and show a screen informing the precondition that must be met
+     * and a button to proceed with the execution.
+     */
+    override fun apply(base: Statement?, description: Description): Statement {
+
+        val postcondition = description.getAnnotation(Postcondition::class.java)
+
+        if (!HALConfig.runningOnEmulator && postcondition != null) {
+
+            val semaphore = Semaphore(0)
+
+            launchActivity(
+                Intent(
+                    InstrumentationRegistry.getInstrumentation().targetContext,
+                    MainActivity::class.java
+                )
+            )
+
+            runOnUiThread {
+                activity.apply {
+                    findViewById<TextView>(R.id.textView_test_title).text = description.methodName
+                    findViewById<TextView>(R.id.textView_message).text =
+                        String.format(getString(R.string.test_message_format_after), postcondition.text)
+                    findViewById<Button>(R.id.button_proceed).setOnClickListener {
+                        semaphore.release()
+                    }
+                }
+            }
+            semaphore.acquire()
+        }
+
+        return super.apply(base, description)
+    }
+}
