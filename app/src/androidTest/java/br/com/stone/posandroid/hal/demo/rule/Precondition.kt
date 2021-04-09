b5b9c30945ed1765b9bdfe6ac1b8cec61b792c45
Gabriel Xavier <gabriel.xs.silva39@gmail.com>
2021-04-09 11:31:52 -0300

    Add parameter before in Precondition.kt

--- app/src/androidTest/java/br/com/stone/posandroid/hal/demo/rule/Precondition.kt
+++ app/src/androidTest/java/br/com/stone/posandroid/hal/demo/rule/Precondition.kt
@@ -1,8 +1,8 @@
 package br.com.stone.posandroid.hal.demo.rule
 
 /**
- * Annotation used to indicate the precondition that must be met before proceeding with test execution.
+ * Annotation used to indicate the precondition that must be met before/after proceeding with test execution.
  *
  * This annotation is used in conjuction with [PreconditionTestRule].
  */
-annotation class Precondition(val text: String)
+annotation class Precondition(val text: String, val before: Boolean = true)
