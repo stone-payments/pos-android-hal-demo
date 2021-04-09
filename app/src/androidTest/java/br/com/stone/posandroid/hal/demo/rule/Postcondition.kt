e8a546e301a55be045f548564527d3fed3195e51
Lucas Martins <lucasmariano.martins@outlook.com>
2021-04-09 15:05:43 -0300

    Unifies preconditions and postconditions test rules

--- app/src/androidTest/java/br/com/stone/posandroid/hal/demo/rule/Postcondition.kt
+++ app/src/androidTest/java/br/com/stone/posandroid/hal/demo/rule/Postcondition.kt
@@ -5,6 +5,6 @@ import androidx.test.rule.ActivityTestRule
 /**
  * Annotation used to indicate the precondition that must be met after proceeding with test execution.
  *
- * This annotation is used in conjuction with [PostconditionTestRule].
+ * This annotation is used in conjuction with [ConditionTestRule].
  */
 annotation class Postcondition(val text: String)
