065a79bcdba9314da475805987692e4ecd6fdfb5
Gabriel Xavier <gabriel.xs.silva39@gmail.com>
2021-04-09 12:23:03 -0300

    refactor documentation precondition

--- app/src/androidTest/java/br/com/stone/posandroid/hal/demo/rule/Precondition.kt
+++ app/src/androidTest/java/br/com/stone/posandroid/hal/demo/rule/Precondition.kt
@@ -1,7 +1,7 @@
 package br.com.stone.posandroid.hal.demo.rule
 
 /**
- * Annotation used to indicate the precondition that must be met before/after proceeding with test execution.
+ * Annotation used to indicate the precondition that must be met before proceeding with test execution.
  *
  * This annotation is used in conjuction with [PreconditionTestRule].
  */
