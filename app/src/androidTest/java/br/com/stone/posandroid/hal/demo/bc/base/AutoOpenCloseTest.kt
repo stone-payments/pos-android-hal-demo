1ba22ef0d76d4e48588bb7453ba9d1c0d2ff7104
Gabriel Xavier <gabriel.xs.silva39@gmail.com>
2021-04-09 16:40:09 -0300

    add support Sunmi test

--- app/src/androidTest/java/br/com/stone/posandroid/hal/demo/bc/base/AutoOpenCloseTest.kt
+++ app/src/androidTest/java/br/com/stone/posandroid/hal/demo/bc/base/AutoOpenCloseTest.kt
@@ -3,6 +3,7 @@ package br.com.stone.posandroid.hal.demo.bc.base
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.platform.app.InstrumentationRegistry
 import br.com.stone.posandroid.hal.api.Properties.KEY_CONTEXT
+import br.com.stone.posandroid.hal.api.Properties.KEY_SUNMI_KEYMAP
 import br.com.stone.posandroid.hal.api.Properties.RESULTS_FILE_KEY
 import br.com.stone.posandroid.hal.api.Properties.RESULTS_KEY
 import br.com.stone.posandroid.hal.api.Properties.TARGET_RESULT_KEY
@@ -40,13 +41,15 @@ abstract class AutoOpenCloseTest {
                     CombinedResult(PinpadResult(CLO, PP_OK))
                 )
             )
-
         pinpad = deviceProvider.getPinpad(
             mutableMapOf(
-                KEY_CONTEXT to context
+                KEY_CONTEXT to context,
+                KEY_SUNMI_KEYMAP to "160331\n161332\n160301\n161302\n"
             ),
+
             mutableMapOf(
                 RuntimeProperties.PinLayout.KEY_PIN_KBD_LAYOUT_ID to R.layout.regularkeyboard,
+                RuntimeProperties.PinLayout.KEY_SUNMI_LAYOUT_INFO to "{\"layout\":${R.layout.regularkeyboard},\"numX\":16,\"numY\":644,\"numW\":172,\"numH\":170,\"lineW\":0,\"cancelX\":532,\"cancelY\":644,\"cancelW\":172,\"cancelH\":170,\"rows\":4,\"clos\":4,\"keymap0\":49,\"keymap1\":50,\"keymap2\":51,\"keymap3\":27,\"keymap4\":52,\"keymap5\":53,\"keymap6\":54,\"keymap7\":12,\"keymap8\":55,\"keymap9\":56,\"keymap10\":57,\"keymap11\":13,\"keymap12\":0,\"keymap13\":48,\"keymap14\":0,\"keymap15\":13}",
                 RESULTS_KEY to queue,
                 TARGET_RESULT_KEY to RESULTS_KEY
             ),
@@ -60,7 +63,9 @@ abstract class AutoOpenCloseTest {
 
     @After
     fun tearDown() {
-        pinpad.runtimeProperties[TARGET_RESULT_KEY] = RESULTS_KEY
-        pinpad.close()
+        if (::pinpad.isInitialized) {
+            pinpad.runtimeProperties[TARGET_RESULT_KEY] = RESULTS_KEY
+            pinpad.close()
+        }
     }
 }
