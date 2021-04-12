4205130cf53dc3d45776324dd97a562cc414a3fe
Gabriel Xavier <gabriel.xs.silva39@gmail.com>
2021-04-12 12:14:51 -0300

    Fix test in CardCommandsTest.kt

--- app/src/androidTest/java/br/com/stone/posandroid/hal/demo/bc/CardCommandsTest.kt
+++ app/src/androidTest/java/br/com/stone/posandroid/hal/demo/bc/CardCommandsTest.kt
@@ -1,39 +1,52 @@
 package br.com.stone.posandroid.hal.demo.bc
 
 import br.com.stone.posandroid.hal.api.Properties.RESULTS_FILE_KEY
-import br.com.stone.posandroid.hal.api.bc.PinpadCallbacks.Companion.INSERT_SWIPE_CARD
 import br.com.stone.posandroid.hal.api.bc.PinpadCallbacks.Companion.PROCESSING
+import br.com.stone.posandroid.hal.api.bc.PinpadCallbacks.Companion.UPDATING_TABLES
 import br.com.stone.posandroid.hal.api.bc.ext.getCardOrThrows
 import br.com.stone.posandroid.hal.api.bc.ext.removeCardOrThrows
 import br.com.stone.posandroid.hal.demo.bc.base.AutoLoadTableTest
+import br.com.stone.posandroid.hal.demo.rule.ConditionTestRule
+import br.com.stone.posandroid.hal.demo.rule.Precondition
 import br.com.stone.posandroid.hal.demo.util.DEFAULT_GCR_INPUT
-import br.com.stone.posandroid.hal.demo.util.VISA_TESTCARD01_OUTPUT
-import io.mockk.verifySequence
+import io.mockk.verifyOrder
 import kotlinx.coroutines.runBlocking
-import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
+import org.junit.FixMethodOrder
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runners.MethodSorters
 
-
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class CardCommandsTest : AutoLoadTableTest() {
 
     private val stubResultsFolder = "resources/card-commands-test"
 
+    @get:Rule
+    val conditionsTestRule = ConditionTestRule()
+
+
     @Test
+    @Precondition("Insert Card")
     fun validateGetCard() = runBlocking {
 
         pinpad.runtimeProperties[RESULTS_FILE_KEY] =
             "$stubResultsFolder/validate_get_card.json"
 
-        assertEquals(VISA_TESTCARD01_OUTPUT, pinpad.getCardOrThrows(DEFAULT_GCR_INPUT))
+        val subject = pinpad.getCardOrThrows(DEFAULT_GCR_INPUT)
 
-        verifySequence {
-            callback.onEvent(INSERT_SWIPE_CARD, "")
+        assertTrue(subject.contains("^0300108\\d{4}\\s{76}".toRegex()))
+
+        verifyOrder {
+            callback.onEvent(UPDATING_TABLES, "")
             callback.onEvent(PROCESSING, "")
+
         }
+
     }
 
     @Test
+    @Precondition("Remove card")
     fun validateRemoveCard() = runBlocking {
 
         pinpad.runtimeProperties[RESULTS_FILE_KEY] =
