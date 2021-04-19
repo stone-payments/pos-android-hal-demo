872f2116f843c0f1ff174fe7a87eef9609500f46
Gabriel Xavier <gabriel.xs.silva39@gmail.com>
2021-04-19 14:44:59 -0300

    Fix Flow Payments test

--- app/src/androidTest/java/br/com/stone/posandroid/hal/demo/bc/PaymentFlowsTest.kt
+++ app/src/androidTest/java/br/com/stone/posandroid/hal/demo/bc/PaymentFlowsTest.kt
@@ -8,41 +8,85 @@ import br.com.stone.posandroid.hal.api.bc.ext.getPINOrThrows
 import br.com.stone.posandroid.hal.api.bc.ext.goOnChipOrThrows
 import br.com.stone.posandroid.hal.api.bc.ext.removeCardOrThrows
 import br.com.stone.posandroid.hal.demo.bc.base.AutoLoadTableTest
+import br.com.stone.posandroid.hal.demo.rule.ConditionTestRule
+import br.com.stone.posandroid.hal.demo.rule.Postcondition
+import br.com.stone.posandroid.hal.demo.rule.Precondition
 import br.com.stone.posandroid.hal.demo.util.DEFAULT_GCR_INPUT
+import br.com.stone.posandroid.hal.demo.util.DEFAULT_GOC_COMMAND
+import br.com.stone.posandroid.hal.demo.util.DEFAULT_LENGTH_CARD_MAG
+import br.com.stone.posandroid.hal.demo.util.REGEX_CARD_CHIP
+import br.com.stone.posandroid.hal.demo.util.REGEX_CARD_MAG
 import br.com.stone.posandroid.hal.demo.util.VISA_TESTCARD01_OUTPUT
 import br.com.stone.posandroid.hal.demo.util.isValidHex
+import io.mockk.verifyOrder
 import io.mockk.verifySequence
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Ignore
+import org.junit.Rule
 import org.junit.Test
 
 class PaymentFlowsTest : AutoLoadTableTest() {
 
     private val stubResultsFolder = "resources/bc/payment-flow-tests"
 
+    @get:Rule
+    val conditionTestRule = ConditionTestRule()
+
     @Test
-    fun validatePaymentWithContactApprovedOnline() = runBlocking {
+    @Precondition("Insert card")
+    @Postcondition("Remove the card after inserting the pin")
+    fun validatePaymentWithContact() = runBlocking {
 
         pinpad.runtimeProperties[RESULTS_FILE_KEY] =
             "$stubResultsFolder/validate_contact_online_approved.json"
 
-        assertEquals(VISA_TESTCARD01_OUTPUT, pinpad.getCardOrThrows(DEFAULT_GCR_INPUT))
+        val subject = pinpad.getCardOrThrows(DEFAULT_GCR_INPUT)
+        assertTrue(subject.contains(REGEX_CARD_CHIP.toRegex()))
+        val chipEMV = pinpad.goOnChipOrThrows(DEFAULT_GOC_COMMAND)
 
-        assertTrue(pinpad.goOnChipOrThrows("").startsWith('2'))
-
+        assertTrue(chipEMV.startsWith('2') || chipEMV.startsWith('1'))
         assertTrue(pinpad.finishChipOrThrows("0000000000000").startsWith('0'))
-
         assertTrue(pinpad.removeCardOrThrows("Remova o cartão").isBlank())
 
-        verifySequence {
-            callback.onEvent(PinpadCallbacks.INSERT_SWIPE_CARD, "")
+        verifyOrder {
             callback.onEvent(PinpadCallbacks.PROCESSING, "")
+            callback.onEvent(PinpadCallbacks.PIN_STARTING, "")
+            callback.onShowPinEntry(any(), any(), any())
+            callback.onEvent(PinpadCallbacks.REMOVE_CARD, "")
         }
     }
 
+
     @Test
+    @Precondition("Swipe the card")
+    fun validatePaymentWithMag() = runBlocking {
+
+        pinpad.runtimeProperties[RESULTS_FILE_KEY] =
+            "$stubResultsFolder/validate_mag_pin_approved.json"
+
+        val subject = pinpad.getCardOrThrows(DEFAULT_GCR_INPUT)
+
+        assertEquals(DEFAULT_LENGTH_CARD_MAG, subject.length)
+        assertTrue(subject.contains(REGEX_CARD_MAG.toRegex()))
+
+        assertTrue(
+            pinpad.getPINOrThrows(
+                "31600000000000000000000000000000000164391998410650011   10412DIGITE SUA SENHA                "
+            ).isValidHex(expectedSize = 36)
+        )
+
+        verifyOrder {
+            callback.onEvent(PinpadCallbacks.UPDATING_TABLES, "")
+            callback.onEvent(PinpadCallbacks.INSERT_SWIPE_CARD, "")
+            callback.onEvent(PinpadCallbacks.PIN_STARTING, "")
+        }
+    }
+
+
+    @Test
+    @Ignore("because you need the specific test card")
     fun validatePaymentWithContactDeniedOnline() = runBlocking {
         pinpad.runtimeProperties[RESULTS_FILE_KEY] =
             "$stubResultsFolder/validate_contact_online_denied.json"
@@ -56,8 +100,9 @@ class PaymentFlowsTest : AutoLoadTableTest() {
         }
     }
 
-    @Ignore
+
     @Test
+    @Ignore("because you need the specific test card")
     fun validatePaymentWithContactDeniedOffline() = runBlocking {
 
         pinpad.runtimeProperties[RESULTS_FILE_KEY] =
@@ -72,32 +117,6 @@ class PaymentFlowsTest : AutoLoadTableTest() {
         verifySequence {
             callback.onEvent(PinpadCallbacks.INSERT_SWIPE_CARD, "")
             callback.onEvent(PinpadCallbacks.PROCESSING, "")
-        }
-    }
-
-    @Ignore
-    @Test
-    fun validatePaymentWithMagApprovedWithPin() = runBlocking {
-
-        pinpad.runtimeProperties[RESULTS_FILE_KEY] =
-            "$stubResultsFolder/validate_mag_pin_approved.json"
-
-        val expectedOutput =
-            "00001010500                                                                            29376436871651006=0305000523966        000                                                                                                        15376436871651006    01AMEX GREEN      246SERGIO SANTOS             05013100                   00000000076000"
-
-        assertEquals(expectedOutput, pinpad.getCardOrThrows(DEFAULT_GCR_INPUT))
-
-        assertTrue(
-            pinpad.getPINOrThrows(
-                "31600000000000000000000000000000000164391998410650011   10412DIGITE SUA SENHA                "
-            ).isValidHex(expectedSize = 36)
-        )
-
-        verifySequence {
-            callback.onEvent(PinpadCallbacks.INSERT_SWIPE_CARD, "")
-            callback.onEvent(PinpadCallbacks.PROCESSING, "")
-            callback.onEvent(PinpadCallbacks.PIN_STARTING, "")
-            callback.onShowPinEntry(any(), any(), any())
         }
     }
 }
--- app/src/androidTest/java/br/com/stone/posandroid/hal/demo/util/CardTestUtils.kt
+++ app/src/androidTest/java/br/com/stone/posandroid/hal/demo/util/CardTestUtils.kt
@@ -2,7 +2,14 @@ package br.com.stone.posandroid.hal.demo.util
 
 import br.com.stone.posandroid.hal.demo.bc.base.AutoLoadTableTest
 
+
+
+const val REGEX_CARD_CHIP = "^0300108\\d{4}\\s{76}"
+const val REGEX_CARD_MAG = "^0000000007"
+const val DEFAULT_GOC_COMMAND = "00000000010000000000000001031600000000000000000000000000000000000000000000000000000000003000003000"
 const val DEFAULT_GCR_INPUT =
     "0801000000023850020904164230${AutoLoadTableTest.TABLE_STUB_TIMESTAMP}000"
 const val VISA_TESTCARD01_OUTPUT =
     "03001080500                                                                            344761739001010119=22122011758928889   000                                                                                                        164761739001010119   01                   VISA ACQUIRER TEST/CARD 0122123100                           840000"
+
+const val DEFAULT_LENGTH_CARD_MAG = 342
