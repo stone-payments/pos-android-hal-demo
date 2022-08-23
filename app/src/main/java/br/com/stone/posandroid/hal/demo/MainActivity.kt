e976d4ab259d8fdca6b3cb0e7d63d00683d936e4
Izaura Silva <izaura.silva@stone.com.br>
2022-08-23 11:38:02 -0300

    Removes button test in main activity

--- app/src/main/java/br/com/stone/posandroid/hal/demo/MainActivity.kt
+++ app/src/main/java/br/com/stone/posandroid/hal/demo/MainActivity.kt
@@ -17,20 +17,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button: Button = findViewById(R.id.button_toggle_home)
-        button.setOnClickListener {
-            thread {
-                val subject =
-                    HALConfig.deviceProvider.getSettings(mapOf(Properties.KEY_CONTEXT to applicationContext))
-                val toggleHomeKeyFunction = subject.toggleHomeKeyFunction(true)
-                runOnUiThread {
-                    Toast.makeText(
-                        applicationContext,
-                        "Desativado $toggleHomeKeyFunction",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-        }
     }
 }
