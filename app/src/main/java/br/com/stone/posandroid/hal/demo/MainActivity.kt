d3e9f11647a9483dfd88203099b2ec013c22eb41
Izaura Silva <izaura.silva@stone.com.br>
2022-08-23 11:40:08 -0300

    Removes button test in main activity

--- app/src/main/java/br/com/stone/posandroid/hal/demo/MainActivity.kt
+++ app/src/main/java/br/com/stone/posandroid/hal/demo/MainActivity.kt
@@ -2,15 +2,6 @@ package br.com.stone.posandroid.hal.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
-import br.com.stone.posandroid.hal.api.Properties
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
